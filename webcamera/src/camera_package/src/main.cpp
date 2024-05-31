599b2b170c9177aa2759d68ec4ed1656571d6e0a
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-05-31 11:12:23 +0900

    ROS2

---
+++ webcamera/src/camera_package/src/main.cpp
@@ -0,0 +1,11 @@
+#include "camera_package/camera_node.hpp"
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CameraNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ webcamera/src/imshow/include/imshow.hpp
@@ -0,0 +1,15 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+#include <opencv2/opencv.hpp>
+
+class ImageReceiverNode : public rclcpp::Node
+{
+public:
+    ImageReceiverNode();
+
+private:
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
